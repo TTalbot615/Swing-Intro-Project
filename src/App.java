05045b13e06a1b9c6b30f3052e36d870b30c90ea
TTalbot615 <ttalbot615@gmail.com>
2019-12-07 23:38:52 -0800

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Hello World"); //New Frame with Name Hello World
+		
+		frame.setSize(600, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Tells application to close when you exit window
+		frame.setVisible(true);
+	}
+
+}
