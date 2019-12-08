9b3a6a6523573cbd8afce4c8ecd4c914b680468a
TTalbot615 <ttalbot615@gmail.com>
2019-12-08 00:12:10 -0800

    Created a basic frame named Hello World that exits when closed

--- src/App.java
+++ src/App.java
@@ -1,14 +1,19 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("Hello World"); //New Frame with Name Hello World
 		
-		frame.setSize(600, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Tells application to close when you exit window
-		frame.setVisible(true);
+		SwingUtilities.invokeLater(new Runnable() { //Added this new class to make it more robust, helps prevent crashing
+			public void run() {
+				JFrame frame = new JFrame("Hello World"); //New Frame with Name Hello World
+				frame.setSize(600, 500);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Tells application to close when you exit window
+				frame.setVisible(true); 
+			}
+		});
+		
 	}
 
 }
